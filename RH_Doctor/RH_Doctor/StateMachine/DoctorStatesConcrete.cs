9bc6e7a4e2495d2deb9e4fc28eb4e02deecb272d
davidVeer <davidvdveer05@gmail.com>
2024-11-09 13:19:34 +0100

    adding States

---
+++ RH_Doctor/RH_Doctor/StateMachine/DoctorStatesConcrete.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RH_Doctor.StateMachine {
+
+    internal class Connect : DoctorStateAbstract {
+    
+    }
+
+    internal class Login : DoctorStateAbstract {
+
+    }
+
+    internal class EnterCommand : DoctorStateAbstract {
+
+    }
+
+    internal class Subscribe : DoctorStateAbstract {
+
+    }
+
+    internal class Unsubscribe : DoctorStateAbstract {
+
+    }
+
+    internal class StartSession : DoctorStateAbstract {
+
+    }
+
+    internal class EndSession : DoctorStateAbstract {
+
+    }
+
+    internal class SendData : DoctorStateAbstract {
+
+    }
+
+    internal class FethData : DoctorStateAbstract {
+
+    }
+
+}
