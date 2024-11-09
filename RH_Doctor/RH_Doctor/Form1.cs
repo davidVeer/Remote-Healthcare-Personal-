1e4935ee919916073877b2e0b9c9a90b7c8503db
davidVeer <davidvdveer05@gmail.com>
2024-11-09 13:19:22 +0100

    adding Forms

--- RH_Doctor/RH_Doctor/Form1.cs
+++
@@ -1,7 +0,0 @@
-namespace RH_Doctor {
-    public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
-    }
-}
--- RH_Doctor/RH_Doctor/Forms/LoginForm.Designer.cs
+++ RH_Doctor/RH_Doctor/Forms/LoginForm.Designer.cs
@@ -1,12 +1,12 @@
-﻿namespace RH_Doctor {
-    partial class Form1 {
-        /// <summary>
-        ///  Required designer variable.
+﻿namespace RH_Doctor.Forms {
+    partial class LoginForm {
+        /// <summary> 
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
+        /// <summary> 
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing) {
@@ -16,17 +16,15 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
         }
 
         #endregion
