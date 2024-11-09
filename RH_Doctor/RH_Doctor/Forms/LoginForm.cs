24b5a5ab5239321b690abde2ebf113bc437feebb
davidVeer <davidvdveer05@gmail.com>
2024-11-09 19:38:08 +0100

    added a running Task to read messages form Login Form

--- RH_Doctor/RH_Doctor/Forms/LoginForm.cs
+++ RH_Doctor/RH_Doctor/Forms/LoginForm.cs
@@ -4,11 +4,12 @@ namespace RH_Doctor.Forms {
     internal partial class LoginForm : UserControl {
         public readonly Form mainForm;
         public readonly ServerConnection serverConnection;
-        
+
         public LoginForm(Form mainForm) {
             InitializeComponent();
             this.mainForm = mainForm;
             serverConnection = new ServerConnection();
+            Task.Run(async () => await serverConnection.ReadMessagesAsync());
         }
 
         private void LoginButton_Click(object sender, EventArgs e) {
