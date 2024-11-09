4a757f44c91056192a2942e10187e5d4db7c9b2a
davidVeer <davidvdveer05@gmail.com>
2024-11-09 17:59:06 +0100

    message communication

--- RH_Server_Simulation/RH_Server_Simulation/Program.cs
+++ RH_Server_Simulation/RH_Server_Simulation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace RH_Server_Simulator {
     internal class Program {
@@ -14,11 +15,13 @@ namespace RH_Server_Simulator {
             while (true) {
                 if (PatientListner.Pending()) {
                     TcpClient Patient = PatientListner.AcceptTcpClient();
+                    MessageCommunication.SendMessage(Patient.GetStream(), "Welcome Patient");
                     Thread PatientThread = new Thread(async () => HandleClient(Patient));
                     PatientThread.Start();
                 }
                 if (DoctorListner.Pending()) {
                     TcpClient doctor = DoctorListner.AcceptTcpClient();
+                    MessageCommunication.SendMessage(doctor.GetStream(), "Enter Login Data");
                     Thread doctorThread = new Thread(async () => HandleClient(doctor));
                     doctorThread.Start();
                 }
@@ -26,8 +29,39 @@ namespace RH_Server_Simulator {
         }
 
         private static void HandleClient(TcpClient tcpClient) {
+            NetworkStream networkStream = tcpClient.GetStream();
             while (tcpClient.Connected) {
-                throw new Exception("no message functionality Yet");
+                String R_message;
+                String S_message;
+                if ((R_message = MessageCommunication.ReceiveMessage(networkStream)) == null) {
+                    continue;
+                }
+
+                Console.WriteLine($"{R_message}");
+                S_message = Console.ReadLine();
+                MessageCommunication.SendMessage(networkStream, S_message);
+            }
+        }
+    }
+
+    public class MessageCommunication {
+        public static string ReceiveMessage(NetworkStream networkStream) {
+            try {
+                var stream = new StreamReader(networkStream, Encoding.ASCII, true, 128);
+                String line = stream.ReadLine();
+                return line;
+            } catch (IOException exception) {
+                return "Connection With Host Was Lost";
+            }
+        }
+
+        public static void SendMessage(NetworkStream networkStream, string message) {
+            try {
+                var stream = new StreamWriter(networkStream, Encoding.ASCII, 128, true);
+                stream.WriteLine(message);
+                stream.Flush();
+            } catch (IOException exception) {
+                return;
             }
         }
     }
