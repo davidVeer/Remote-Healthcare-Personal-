6b152509944ae316e70c7eaa0f69c7c1398076a3
davidVeer <davidvdveer05@gmail.com>
2024-11-09 17:43:35 +0100

    Added Server Connection

--- RH_Server_Simulation/RH_Server_Simulation/Program.cs
+++ RH_Server_Simulation/RH_Server_Simulation/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace RH_Server_Simulator {
+    internal class Program {
+        public static void Main(string[] args) {
+            TcpListener PatientListner = new TcpListener(IPAddress.Any, 4789);
+            TcpListener DoctorListner = new TcpListener(IPAddress.Any, 4790);
+            PatientListner.Start();
+            DoctorListner.Start();
+
+            Console.WriteLine("Starting up server and waiting for connections.....");
+
+            while (true) {
+                if (PatientListner.Pending()) {
+                    TcpClient Patient = PatientListner.AcceptTcpClient();
+                    Thread PatientThread = new Thread(async () => HandleClient(Patient));
+                    PatientThread.Start();
+                }
+                if (DoctorListner.Pending()) {
+                    TcpClient doctor = DoctorListner.AcceptTcpClient();
+                    Thread doctorThread = new Thread(async () => HandleClient(doctor));
+                    doctorThread.Start();
+                }
+            }
+        }
+
+        private static void HandleClient(TcpClient tcpClient) {
+            while (tcpClient.Connected) {
+                throw new Exception("no message functionality Yet");
+            }
+        }
+    }
+}
