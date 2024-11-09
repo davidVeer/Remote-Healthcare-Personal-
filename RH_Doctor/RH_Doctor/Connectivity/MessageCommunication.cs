8043d466729aacdc572dcfec3ce369cca0259d8c
davidVeer <davidvdveer05@gmail.com>
2024-11-09 18:15:32 +0100

    added Sending and Recieving

--- RH_Doctor/RH_Doctor/Connectivity/MessageCommunication.cs
+++ RH_Doctor/RH_Doctor/Connectivity/MessageCommunication.cs
@@ -5,17 +5,30 @@ using System.Net.Sockets;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml.Serialization;
 
 namespace RH_Doctor.Connectivity {
     internal class MessageCommunication {
 
-        public static void SendMessage(TcpClient senderClient, String MessageToSend) { 
-            throw new NotImplementedException();
+        public static void SendMessage(NetworkStream sendingStream, String MessageToSend) {
+            try {
+                var stream = new StreamWriter(sendingStream, Encoding.ASCII, 128, true);
+                stream.WriteLine(MessageToSend);
+                stream.Flush();
+            } catch (IOException exception) {
+                return;
+            }
         }
 
-        public static void RecieveMessage(TcpClient senderClient, String MessageToSend) {
-            throw new NotImplementedException();
+        public static String RecieveMessage(NetworkStream recievingStream, String MessageToSend) {
+            try {
+                var stream = new StreamReader(recievingStream, Encoding.ASCII, true, 128);
+                String line = stream.ReadLine();
+                return line;
+            } catch (IOException exception) {
+                return "Connection With Host Was Lost";
+            }
         }
     }
 }
