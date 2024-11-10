87877fbbb5c778c0c859067dd83df9ea2a3d7fad
davidVeer <davidvdveer05@gmail.com>
2024-11-11 00:58:46 +0100

    option for saving names of Patients for pattern

--- RH_Doctor/RH_Doctor/StateMachine/DoctorStateAbstract.cs
+++ RH_Doctor/RH_Doctor/StateMachine/DoctorStateAbstract.cs
@@ -1,18 +1,20 @@
 ﻿using RH_Doctor.Connectivity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace RH_Doctor.StateMachine {
     internal abstract class DoctorStateAbstract {
         protected ServerConnection protocol;
         protected readonly Regex JsonRegex = new Regex("^\\{.+\\}$");
+        protected readonly List<String> names;
 
         public DoctorStateAbstract(ServerConnection protocol) {
             this.protocol = protocol;
+            this.names = [];
+        }
+
+        protected DoctorStateAbstract(ServerConnection protocol, List<string> names){
+            this.protocol = protocol;
+            this.names = names;
         }
 
         public abstract void PerformAction(String ActionCommand);
